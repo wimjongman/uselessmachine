085cc81159c2b68bea866b337043c6e6ff0f6ab4
Sam Jongman <sam.jongman@weltevree.com>
2016-09-04 00:01:53 +0100

    random gefixt

--- arduino/com.weltevree.uselessmachine/Switch.ino
+++ arduino/com.weltevree.uselessmachine/Switch.ino
@@ -6,10 +6,12 @@ Servo servo;
 int inPin = 2;         // the number of the input pin
 int outPin = 13;       // the number of the output pin
 int servoPin = 9;       // the servo pin
+int gedrag = 0;       // the servo pin
 
 int state = HIGH;      // the current state of the output pin
 int reading;           // the current reading from the input pin
 int servoState = 0;    // the position in degrees of the servo
+int randomGedrag = random(1, 4);
 
 // the follow variables are long's because the time, measured in miliseconds,
 // will quickly become a bigger number than can be stored in an int.
@@ -25,22 +27,37 @@ void setup() {
 
 void loop() {
 
-	reading = digitalRead(inPin);
 	boolean canflip = (millis() - time > debounce); // debounce
 	if (canflip) {
-		if (state == HIGH && reading == LOW)
+		reading = digitalRead(inPin);
+		if (state == HIGH && reading == LOW) {
 			setLow();
-		else if (state == LOW && reading == HIGH)
+			saveTime();
+		} else if (state == LOW && reading == HIGH) {
 			setHigh();
-
-		time = millis();
+			saveTime();
+		}
 	}
 }
 
+void saveTime() {
+	time = millis();
+}
+
 void setHigh() {
+	int randomGedrag = random(1, 4);
 	state = HIGH;
 	digitalWrite(outPin, HIGH);
-	servo.write(90);
+	if (randomGedrag == 1) {
+		gedrag1();
+		return;
+	} else if (randomGedrag == 2) {
+		gedrag2();
+		return;
+	} else if (randomGedrag == 3) {
+		gedrag3();
+		return;
+	}
 }
 
 void setLow() {
@@ -48,4 +65,22 @@ void setLow() {
 	state = LOW;
 	digitalWrite(outPin, LOW);
 	servo.write(0);
+}
+
+void gedrag1() {
+	servo.write(90);
+}
+
+void gedrag2() {
+	servo.write(50);
+	delay(1000);
+	servo.write(90);
+}
+
+void gedrag3() {
+	servo.write(60);
+	delay(1000);
+	servo.write(0);
+	delay(1000);
+	servo.write(90);
 }
